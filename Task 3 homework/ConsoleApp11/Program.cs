17e6c6da61ccd24e41244daac9d28de739b55c5f
IgorStefanovski <47919167+IgorStefanovski@users.noreply.github.com>
2019-03-05 16:38:21 +0100

    Task 3 domashna zadacha

---
+++ Task 3 homework/ConsoleApp11/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[20];
+
+            for (int y = 0; y < 20; y++)
+            {
+                Console.WriteLine("Input Number");
+                nums[y] = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine("The elements of the array :");
+
+
+            foreach (int z in nums)
+            {
+                if (z == 0)
+                {
+                    Console.WriteLine("skipped");
+                    continue;
+
+                }
+                if (z > 99)
+                    break;
+                Console.WriteLine(z);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
