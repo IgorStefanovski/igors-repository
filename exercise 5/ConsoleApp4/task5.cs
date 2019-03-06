78bd97e17d5ab6975f0d3d2776a42ca10da9baad
IgorStefanovski <47919167+IgorStefanovski@users.noreply.github.com>
2019-03-06 14:02:58 +0100

    homework task 2, task 3

---
+++ exercise 5/ConsoleApp4/task5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[5];
+
+            for (int y = 0; y < 5; y++)
+            {
+                Console.WriteLine("Input Number");
+                nums[y] = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine("Sum of the array :");
+
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum += nums[i];
+            }
+
+            {
+
+                Console.WriteLine(sum);
+
+            }
+
+
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+}
+
+        
+    
+
---
+++ task 2 homework/ConsoleApp11/task2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[20];
+
+            for (int y = 0; y < 20; y++)
+            {
+                Console.WriteLine("Input Number");
+                nums[y] = int.Parse(Console.ReadLine());
+
+
+            }
+            Console.WriteLine("The elements of the array :");
+
+
+            foreach (int z in nums)
+            {
+                Console.WriteLine(z);
+            }
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
+        
