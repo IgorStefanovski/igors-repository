17417a32e78aba18fc4ceb526ba9a71a5736f3ed
Code <code@sedc.mk>
2019-03-13 20:48:42 +0100

    Workshop class 6

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string user = "y";
+            do
+            {
+
+
+                Console.WriteLine("Enter a number");
+                string sometstring = (Console.ReadLine());
+                int number;
+
+                if(int.TryParse(sometstring, out number))
+                {
+                    Console.WriteLine(NumberOrNot(number));
+                    Console.WriteLine($"Stats for number {number}");
+                    Console.WriteLine(PostiveOrNegative(number));
+                    Console.WriteLine(OddOrEven(number));
+                }
+                else
+                {
+                    Console.WriteLine("This is not a number");
+                }
+
+
+                Console.WriteLine("Do you want to input another number");
+                user =Console.ReadLine();
+          
+            
+            } while (user =="y");
+            Console.ReadLine();
+
+        }
+
+        public static string NumberOrNot(int number)
+        {
+            if (number.GetType() != typeof(int))
+            {
+                return "This is not a number";
+
+
+            }
+          
+
+                return "This is a number";
+
+
+
+        }
+
+        public static string PostiveOrNegative(int number)
+        {
+            if (number < 0)
+            {
+                return $"Negative";
+
+            }
+            if (number > 0)
+            {
+                return $"Positive";
+
+            }
+
+            return "The number is zero";
+        }
+
+        public static string OddOrEven(int number)
+        {
+
+            if (number % 2 == 0)
+            {
+                return $"Even";
+
+            }
+
+            return $"Odd";
+
+        }
+
+
+        
+
+ }
+    }
+
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2.Entities;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User[] userArray = new User[5];
+
+            userArray[0] = new User(17, "jimm", "333");
+            userArray[1] = new User(18, "limeni", "444");
+            userArray[2] = new User(20, "john", "123");
+            userArray[3] = new User(21, "jimmy", "255");
+            userArray[4] = new User(22, "luke", "723");
+
+            Console.WriteLine("Enter user name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            string password = Console.ReadLine();
+
+           for( int i = 0; i < userArray.Length; i++)
+            {
+                if(userName == userArray[i].Username)
+                {
+                    if(password == userArray[i].Password)
+                    {
+                        Console.WriteLine("You are logged in!");
+                        Console.ReadLine();
+                    }
+                }
+                
+
+            }
+                
+
+              
+
+        
+        }
+    }
+}
